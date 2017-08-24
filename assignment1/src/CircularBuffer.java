c07352b8f14306cd322e1c6578ff69d366e93a2a
Roy Portas <royportas@gmail.com>
2017-08-25 09:01:25 +1000

    Worked on a1

--- assignment1/src/CircularBuffer.java
+++ assignment1/src/CircularBuffer.java
@@ -8,5 +8,53 @@
  */
 
 public class CircularBuffer {
-    
+
+    private int bufferSize;
+    private char[] buffer;
+
+    private int in;
+    private int out;
+
+    /**
+     * Creates the circular buffer
+     * @param size The size of the buffer
+     */
+    public CircularBuffer(int size) {
+        bufferSize = size;
+        buffer = new char[bufferSize];
+
+        in = 0;
+        out = 0;
+    }
+
+    /**
+     * Adds an item to the buffer
+     *
+     * @param item The item to add
+     */
+    public void addItem(char item) {
+        while (out == (in + 1) % bufferSize) {
+            // Wait here
+            // https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/locks/Condition.html
+        }
+
+        buffer[in] = item;
+        in = (in+1) % bufferSize;
+    }
+
+    /**
+     * Gets an item from the buffer
+     *
+     * @returns The item
+     */
+    public char getItem() {
+        while (in == out) {
+            // Wait here
+            // https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/locks/Condition.html
+        }
+
+        char c = buffer[out];
+        out = (out+1) % bufferSize;
+        return c;
+    }
 }
--- assignment1/src/FileConverter.java
+++ assignment1/src/FileConverter.java
@@ -8,7 +8,22 @@
 
 public class FileConverter {
     
+    private static A1Reader reader;
+    private A1Writer writer;
+
     public static void main(String[] args) {
-        System.out.println("Starting FileConverter");
+
+        try {
+            reader = new A1Reader("resources/source.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        CircularBuffer b = new CircularBuffer(20);
+        b.addItem('a');
+        b.addItem('b');
+
+        System.out.println(b.getItem());
+        System.out.println(b.getItem());
     }
 }
