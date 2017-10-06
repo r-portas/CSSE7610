5398bc551886f33f245082f55fa3fd92afb4b1e8
Roy Portas <royportas@gmail.com>
2017-10-07 09:56:38 +1000

    Worked on java code

--- assignment2/java/src/Main.java
+++ assignment2/java/src/Main.java
@@ -1,17 +1,60 @@
 import java.lang.Thread;
-import java.util.concurrent.Semaphore;
+import java.util.Random;
+
+class Monitor {
+    /* The number of writers waiting for the lock */
+    private int writersWaiting;
+    /* The lock on the variables x1 and x2 */
+    private boolean locked;
+
+    public synchronized void startWrite() {
+        writersWaiting++;
+
+        while (locked) {
+            try {
+                wait();
+            } catch (InterruptedException e) {}
+        }
+        locked = true;
+        writersWaiting--;
+        notifyAll();
+    }
+
+    public synchronized void endWrite() {
+        locked = false;
+        notifyAll();
+    }
+
+    public synchronized boolean startIncrement() {
+        if (writersWaiting == 0 && locked == false) {
+            locked = true;
+            notifyAll();
+            return true;
+        }
+        notifyAll();
+        return false;
+    }
+
+    public synchronized void endIncrement() {
+        locked = false;
+        notifyAll();
+    }
+}
 
 class Shared {
     public int c = 0;
     public int x1 = 0;
     public int x2 = 0;
-    public int waitingWriters = 0;
 
-    public Semaphore sem;
+    public Monitor mon;
+    public Random rand;
 
     public Shared() {
-        // A binary semaphore
-        sem = new Semaphore(1);
+        // The monitor;
+        mon = new Monitor();
+
+        // Specify a seed for consistency
+        rand = new Random(1);
     }
 }
 
@@ -30,7 +73,53 @@ class Reader extends MyThread {
     int d1;
     int d2;
 
+    int lastRead;
+
     public Reader(int id, Shared s) {
+        super(id, s);
+        c0 = 0;
+        d1 = 0;
+        d2 = 0;
+        lastRead = -1;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                // Wait a random amount of time
+                Thread.sleep(shared.rand.nextInt(10));
+
+
+                do {
+                    do {
+                        c0 = shared.c;
+                    } while (c0 % 2 != 0);
+                    
+                    d1 = shared.x1;
+                    d2 = shared.x2;
+                } while (c0 != shared.c);
+
+                if (lastRead == c0) {
+                    // End the reader
+                    break;
+                }
+                lastRead = c0;
+
+                A2Event.readData(id, shared.x1, shared.x2);
+
+            } catch (InterruptedException e) {
+                System.err.print(e);
+            }
+        }
+    }
+}
+
+class Incrementer extends MyThread {
+    int c0;
+    int d1;
+    int d2;
+
+    public Incrementer(int id, Shared s) {
         super(id, s);
         c0 = 0;
         d1 = 0;
@@ -39,16 +128,36 @@ class Reader extends MyThread {
 
     public void run() {
         while (true) {
-            do {
+            try {
+                // Wait a random amount of time
+                Thread.sleep(shared.rand.nextInt(10));
+
                 do {
                     c0 = shared.c;
                 } while (c0 % 2 != 0);
                 
                 d1 = shared.x1;
                 d2 = shared.x2;
-            } while (c0 != shared.c);
 
-            A2Event.readData(id, shared.x1, shared.x2);
+                if (shared.mon.startIncrement()) {
+                    if (c0 == shared.c) {
+                        shared.c++;
+
+                        shared.x1 = d1 + 1;
+                        shared.x2 = d2 + 1;
+                        A2Event.incrementData(id, shared.x1, shared.x2);
+
+                        shared.c++;
+                        shared.mon.endIncrement();
+                        break;
+                    } else {
+                        shared.mon.endIncrement();
+                    }
+                }
+
+            } catch (InterruptedException e) {
+                System.err.print(e);
+            }
         }
     }
 }
@@ -64,24 +173,25 @@ class Writer extends MyThread {
     }
 
     public void run() {
-        while (true) {
-            try {
-                shared.waitingWriters++;
-                shared.sem.acquire();
+        try {
+            // Wait a random amount of time
+            Thread.sleep(shared.rand.nextInt(10));
 
-                // Figure out how to get the values
-                d1 = shared.x1 + 1;
-                d2 = shared.x1 + 1;
+            shared.mon.startWrite();
+            shared.c++;
 
-                shared.x1 = d1;
-                shared.x2 = d2;
-                A2Event.writeData(id, shared.x1, shared.x2);
+            // Figure out how to get the values
+            d1 = shared.x1 + 1;
+            d2 = shared.x1 + 1;
 
-                shared.sem.release();
-                shared.waitingWriters--;
-            } catch (InterruptedException e) {
-                System.err.print(e);
-            }
+            shared.x1 = d1;
+            shared.x2 = d2;
+            A2Event.writeData(id, shared.x1, shared.x2);
+
+            shared.c++;
+            shared.mon.endWrite();
+        } catch (InterruptedException e) {
+            System.err.print(e);
         }
     }
 }
@@ -89,11 +199,35 @@ class Writer extends MyThread {
 class Main {
     public static void main(String[] args) {
         Shared s = new Shared();
-        
-        Reader r1 = new Reader(1, s);
-        r1.start();
 
-        Writer w1 = new Writer(1, s);
-        w1.start();
+        int numReaders = 5;
+        int numWriters = 5;
+        int numIncrementers = 5;
+        
+        Reader[] readers = new Reader[numReaders];
+        for (int i = 0; i < numReaders; i++) {
+            readers[i] = new Reader(i, s);
+            readers[i].start();
+        }
+
+        Writer[] writers = new Writer[numWriters];
+        for (int i = 0; i < numWriters; i++) {
+            writers[i] = new Writer(i, s);
+            writers[i].start();
+        }
+
+        Incrementer[] incrementers = new Incrementer[numIncrementers];
+        for (int i = 0; i < numIncrementers; i++) {
+            incrementers[i] = new Incrementer(i, s);
+            incrementers[i].start();
+        }
+
+        for (int i = 0; i < numReaders; i++) {
+            try {
+                readers[i].join();
+            } catch (InterruptedException e) {}
+        }
+    
+        A2Event.terminateReadWrite();
     }
 }
